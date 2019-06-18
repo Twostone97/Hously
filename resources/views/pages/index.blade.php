4c1d46277e9be28770a18dade67b707c9a7edd3e
frodobaggins23 <46576318+frodobaggins23@users.noreply.github.com>
2019-06-18 18:20:18 +0200

    homepage stub completed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,9 +4,28 @@
 @endsection 
 
 @section ('styles') 
-<link rel="stylesheet" href="css/index.css">
+<style>
+    body{
+        background-image: url('/img/background-homepage.jpg');
+    }
+    
+</style>
 @endsection
 
 @section ('content')
-<h1>Welcome to Hously</h1>
+<section class="container index__container">
+    <h1>Welcome to Hously</h1>
+    <p>Meet a great app which will make your life as resident much easier</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad adipisci provident doloremque quibusdam eveniet aut autem? Reprehenderit magnam ducimus ipsum explicabo consequuntur, atque laudantium rerum, eveniet porro quam enim ipsam!
+    Amet illum repellat iste numquam tenetur sunt unde vitae ratione possimus dolorum at dolorem eveniet enim aliquam, voluptate aperiam. Amet voluptas expedita sit reiciendis qui, quidem totam sunt laborum assumenda!
+    Laboriosam, a mollitia voluptatem magnam, ducimus reiciendis doloribus dolorem consequatur cumque omnis quam distinctio obcaecati repellendus, minus provident? Et distinctio vero at officiis laborum accusamus magni debitis doloribus voluptatem recusandae!
+    Reiciendis, provident? Fuga nisi, repellat doloribus nihil vero doloremque adipisci vitae nam fugiat illo commodi consequuntur dolores delectus omnis nostrum soluta pariatur laudantium earum eveniet neque, culpa odit, eligendi totam.
+    Ab aliquam reprehenderit, repudiandae in pariatur dicta repellat autem numquam. Ab ratione temporibus, perspiciatis maiores eum nisi, blanditiis reprehenderit itaque placeat quos consequatur totam sequi. Voluptatum sunt nam saepe! Magni?
+    Deserunt dolor maxime quae expedita, officia fugiat eaque cumque quia ex ad ipsam aliquid vitae vel ipsum esse, voluptates eos dignissimos porro sint molestiae aliquam enim quasi? Ea, fuga dolor!
+    Doloribus quod expedita eius, mollitia maiores quam pariatur? Neque error omnis cupiditate ea atque hic labore vitae officia necessitatibus quas quo magni, ad dignissimos deserunt quisquam tenetur autem porro reiciendis.
+    Accusantium dolor sequi ut odit eum aliquid autem vero magnam ullam numquam nemo eligendi ad dolorum amet omnis praesentium ipsa explicabo magni beatae voluptates, reiciendis ipsum neque eaque earum! Perspiciatis?
+    Placeat reiciendis error aperiam quidem facere sint dolore consequatur, aliquam necessitatibus suscipit pariatur exercitationem. Obcaecati, voluptate? Expedita aut reprehenderit dicta eaque, consequatur fuga omnis! Ad ullam praesentium accusamus quo vitae!
+    Soluta quo reprehenderit quibusdam quas rem aliquam officia exercitationem architecto dolores quia dignissimos velit animi sapiente laborum iusto explicabo placeat, id porro quaerat nobis aspernatur excepturi praesentium labore? Quod, totam!</p>
+</section>
+
 @endsection
