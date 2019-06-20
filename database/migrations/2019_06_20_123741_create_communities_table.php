ec3b1bd6f19ddc1188a338e443a650ab566e052c
David Řehoř <rehord@email.cz>
2019-06-20 14:59:30 +0200

    Základy migrací připraveny

--- database/migrations/2019_06_20_123741_create_communities_table.php
+++ database/migrations/2019_06_20_123741_create_communities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOwnersTable extends Migration
+class CreateCommunitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateOwnersTable extends Migration
      */
     public function up()
     {
-        Schema::create('owners', function (Blueprint $table) {
+        Schema::create('communities', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('chat_id');
             $table->unsignedBigInteger('building_id');
+            $table->text('community_name');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateOwnersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('owners');
+        Schema::dropIfExists('communities');
     }
 }
--- database/migrations/2019_06_20_124940_create_noticeboards_table.php
+++ database/migrations/2019_06_20_124940_create_noticeboards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOwnersTable extends Migration
+class CreateNoticeboardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateOwnersTable extends Migration
      */
     public function up()
     {
-        Schema::create('owners', function (Blueprint $table) {
+        Schema::create('noticeboards', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('building_id');
             $table->timestamps();
         });
@@ -28,6 +27,6 @@ class CreateOwnersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('owners');
+        Schema::dropIfExists('noticeboards');
     }
 }
