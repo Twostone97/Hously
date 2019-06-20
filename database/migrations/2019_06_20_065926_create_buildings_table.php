1ab13b2f4b14a9e27254b0114f989c5a3efb6b03
David Řehoř <rehord@email.cz>
2019-06-20 11:11:20 +0200

    Příprava migrací

---
+++ database/migrations/2019_06_20_065926_create_buildings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('city');
+            $table->string('street');
+            $table->integer('house_number');
+            $table->integer('postal');
+            $table->unsignedBigInteger('owner_id');
+            $table->date('construction_date');
+            $table->integer('floors_above_ground');
+            $table->integer('floors_bellow_ground');
+            $table->boolean('heating');
+            $table->boolean('gas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
