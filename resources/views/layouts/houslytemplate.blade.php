819a4623899c97da56e17d2cf64252c87822c636
frodobaggins23 <46576318+frodobaggins23@users.noreply.github.com>
2019-06-23 14:19:25 +0200

    homepage continued

--- resources/views/layouts/houslytemplate.blade.php
+++ resources/views/layouts/houslytemplate.blade.php
@@ -32,67 +32,59 @@
                 <div class="header__logo">
                     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" height="100%" width="100%" viewBox="0 50 300 200"><g id="svg-container" transform="matrix(2.86,0,0,2.86,-51.253101139068605,165.89496242523194)"><g id="surrounding_shape" transform="matrix(1,0,0,1,24.818958282470703,-23.578244795799257)"><rect width="40.14220590267181" height="37.68726023985545" fill="transparent" stroke-width="2" stroke="#da5100" data-shape-id="custom" surrounded-groups="symbols" fill-type="outline" id="surrounding_shape_0"></rect></g><g id="headline" transform="matrix(1,0,0,1,5.5734710693359375,0)"><text data-font-id="1077" font-family="playfair_italic" id="headline_0" transform="matrix(1,0,0,1,63.48461538461538,0)" svgjs:data="{&quot;leading&quot;:&quot;1.3&quot;}"><tspan id="headline_0_word_0" fill="#000000" svgjs:data="{&quot;leading&quot;:&quot;1.3&quot;}">Hously</tspan></text></g><g id="symbols" transform="matrix(2.428571387918504,0,0,2.428571387918504,31.39018435546491,7.921807048397751)"><text data-font-id="1249" font-family="hero__bold" id="symbols_0" transform="matrix(1.074698580771455,0,0,1.074698580771455,-0.4108421942430027,0.4108421942430027)" svgjs:data="{&quot;leading&quot;:&quot;1.3&quot;}"><tspan id="symbols_0_word_0" fill="#517ca4" svgjs:data="{&quot;leading&quot;:&quot;1.3&quot;}">H</tspan></text></g></g><defs><style>@font-face {font-family: playfair_italic;src: url(https://pismo.tailorbrands.com/v2/px_fonts/playfair_italic)}</style><style>@font-face {font-family: hero__bold;src: url(https://pismo.tailorbrands.com/v2/px_fonts/hero__bold)}</style></defs></svg>     
                 </div>
-                @guest
+                
                 <div class="header__login">
-                    <p><strong>Log in</strong></p>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="email" required autocomplete="email" autofocus>
-                        @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                        @enderror
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autocomplete="current-password">
-                        @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                        @enderror
-                        
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Login') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                        <br>    
-                        <a class="btn btn-link text-light" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </form>
-                </div>
-                <div class="header__register">
-                        <p><strong>Not yet a member?</strong></p>
-                        <a href="register"><button class="btn btn-primary">Register</button></a>
-                </div>
-            
-                
-                @else
-                <div class="header__login">    
-                    <div class="header__login__authenticated">
-                        <ul>
-                            <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-left" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->first_name }} <span class="caret"></span>
+                    @guest
+                    <div class="header__login__guest">
+                        <div class="header__login__guest__log">
+                            <p><strong>Log in</strong></p>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="email" required autocomplete="email" autofocus>
+                                @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autocomplete="current-password">
+                                @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                                
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+    
+                                @if (Route::has('password.request'))
+                                <br>    
+                                <a class="btn btn-link text-light" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
                                     </a>
-                
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                        </ul>
+                                @endif
+                            </form>
+                        </div>
+                        <div class="header__login__guest__reg">
+                                <p><strong>Not yet a member?</strong></p>
+                                <a href="register"><button class="btn btn-primary">Register</button></a>
+                        </div>
                     </div>
+                    @else
+                    <div class="header__login__auth">    
+                        {{ Auth::user()->first_name }}
+                        <a href="{{ route('logout') }}"
+                            onclick="
+                            event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                        </form>                            
+                    </div>
+                    @endguest
                 </div>
-                @endguest
             
         </div>
         <div class="navigation">
