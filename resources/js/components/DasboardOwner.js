8592f7c18fbcf1490ae38e9f6da50d281948c4eb
unknown <timko.michal@centrum.cz>
2019-07-14 21:02:06 +0200

    rozpracovany dashboard

--- resources/js/components/DasboardOwner.js
+++ resources/js/components/DasboardOwner.js
@@ -1,36 +1,22 @@
 import React, { useState, useEffect } from "react";
-import UserReg from "./admin/userReg.js";
-import BuildingReg from "./admin/buildingReg.js";
-import BuildingInfo from "./admin/buildingInfo.js";
-import UserList from "./admin/UserList.js";
-import UserDetail from "./admin/userDetail.js";
+
+import BuildingInfo from "./owner/buildingInfo.js";
+import UserListOwner from "./owner/userListOwner.js";
 
 const DashboardOwner = ({ apidata }) => {
     console.log("apidata", apidata);
 
-    const [isDetail, setIsdetail] = useState(false);
-    const [detail_id, setDetail_id] = useState(null);
+    // const [isDetail, setIsdetail] = useState(false);
+    // const [detail_id, setDetail_id] = useState(null);
 
-    const handleSetDetail = input => {
-        setIsdetail(!isDetail);
-        setDetail_id(input);
-    };
+    // const handleSetDetail = input => {
+    //     setIsdetail(!isDetail);
+    //     setDetail_id(input);
+    // };
 
     return (
         <>
             <div>
-                <div className=" page__main__dash__item i__big">
-                    <UserReg apidata={apidata} />
-                </div>
-                <div className=" page__main__dash__item i__big">
-                    <BuildingReg
-                        data={apidata.this_building}
-                        owners={apidata.owners}
-                        users={apidata.users}
-                        flats={apidata.flats}
-                        residents={apidata.residents}
-                    />
-                </div>
                 <div className=" page__main__dash__item i__big">
                     <BuildingInfo
                         data={apidata.this_building}
@@ -41,24 +27,12 @@ const DashboardOwner = ({ apidata }) => {
                     />
                 </div>
                 <div className=" page__main__dash__item i__big">
-                    {!isDetail ? (
-                        <UserList
-                            residents={apidata.residents}
-                            users={apidata.users}
-                            flats={apidata.flats}
-                            handleSetDetail={handleSetDetail}
-                        />
-                    ) : (
-                        <UserDetail
-                            handleSetDetail={handleSetDetail}
-                            user={apidata.users[detail_id - 1]}
-                            resident={apidata.residents.filter(
-                                resident => resident.user_id == detail_id
-                            )}
-                            rentcontracts={apidata.rentcontracts}
-                            flats={apidata.flats}
-                        />
-                    )}
+                    <UserListOwner
+                        residents={apidata.residents}
+                        users={apidata.users}
+                        flats={apidata.flats}
+                        handleSetDetail={handleSetDetail}
+                    />
                 </div>
 
                 <div className="page__main__dash__item i__small">
@@ -70,4 +44,4 @@ const DashboardOwner = ({ apidata }) => {
     );
 };
 
-export default DashboardOwner ;
+export default DashboardOwner;
