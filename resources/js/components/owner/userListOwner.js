40767264317bf3aa4d69b7a52cfaeba9f4c7f50a
David Řehoř <rehord@email.cz>
2019-07-18 16:41:05 +0200

    dash owner fix

--- resources/js/components/owner/userListOwner.js
+++ resources/js/components/owner/userListOwner.js
@@ -3,52 +3,61 @@ const UserListOwner = ({ residents, users, flats }) => {
     return (
         <>
             <div className="page__main__dash__item i__full">
-                <div className="page__main__dash__item__head" />
-                <h3>Databaze obyvatel</h3>
-            </div>
-            <div className="page__main__dash__item__body" />
-            <ol>
-                {residents.map(resident => {
-                    return (
-                        <li>
-                            <div>
-                                {`Name: ` +
-                                    `${
-                                        users[resident.user_id - 1].first_name
-                                    }` +
-                                    ` ` +
-                                    `${users[resident.user_id - 1].last_name}`}
-                            </div>
+                <div className="page__main__dash__item__head">
+                    <h3>Database of Residents</h3>
+                </div>
+                <div className="page__main__dash__item__body">
+                    <ol className="resident__list__container">
+                        {residents.map(resident => {
+                            return (
+                                <li>
+                                    <div>
+                                        <strong>
+                                            {
+                                                users[resident.user_id - 1]
+                                                    .first_name
+                                            }{" "}
+                                            {
+                                                users[resident.user_id - 1]
+                                                    .last_name
+                                            }
+                                        </strong>
+                                    </div>
 
-                            <div>
-                                {flats.map(flat => {
-                                    if (flat.id == resident.flat_id)
-                                        return (
-                                            <>
-                                                {"Flat number:" +
-                                                    `${flat.number}`}
-                                            </>
-                                        );
-                                })}
-                            </div>
-                            <div>
-                                {`Contact: ` +
-                                    `${users[resident.user_id - 1].email}`}
-                            </div>
-                            <div>
-                                <a
-                                    href={`/storage/contract/${
-                                        resident.user_id
-                                    }.pdf`}
-                                    target="_blank"
-                                >
-                                    Nájemní Smlouva
-                                </a>
-                            </div>
-                        </li>
-                    );
-                })}
-            </ol>
+                                    <div>
+                                        {flats.map(flat => {
+                                            if (flat.id == resident.flat_id)
+                                                return (
+                                                    <>
+                                                        {"Flat number:" +
+                                                            `${flat.number}`}
+                                                    </>
+                                                );
+                                        })}
+                                    </div>
+                                    <div>
+                                        {`Contact: ` +
+                                            `${
+                                                users[resident.user_id - 1]
+                                                    .email
+                                            }`}
+                                    </div>
+                                    <div>
+                                        <a
+                                            href={`/storage/contract/${
+                                                resident.flat_id
+                                            }.pdf`}
+                                            target="_blank"
+                                        >
+                                            Nájemní Smlouva
+                                        </a>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ol>
+                </div>
+            </div>
         </>
     );
 };
