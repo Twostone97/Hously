3d87e7023d46c41d56ec2bc30306765c5b4a6dc4
frodobaggins23 <46576318+frodobaggins23@users.noreply.github.com>
2019-07-18 21:39:23 +0200

    available flats styling done

--- resources/views/pages/flats.blade.php
+++ resources/views/pages/flats.blade.php
@@ -20,58 +20,52 @@ Available flats
 
 @section ('content')
 
-<main class="bg__wall">
+<main class="bg__tertiary">
     <section class="page__main bg__gradient-light">
         <div class="page__main__promo">
             <h1>Available flats</h1>
             <h5>Currently you can choose from following flats that are managed via Hously...</h5>
         </div>
-        <div class="page__main__flat__container">
 
+        <div class="page__main__flat">
             @foreach ($allbuildings as $building)
+            <h3> Building location: {{$building->street}} {{$building->house_number}}</h3>
+            <div class="page__main__flat__container">
 
 
-            <div class="page__main__flat__container__item">
 
                 @foreach ($list_of_flats as $one_flat)
                 @if ($building->id === $one_flat->building_id)
+                <div class="page__main__flat__container__item">
+                    <div class="page__main__flat__container__item__head">
+                        Flat no. {{$one_flat->number}}
+                    </div>
 
-                <div class="page__main__flat__container__item__head">
-                    {{$building->street}} {{$building->house_number}}
-                    {{$one_flat->number}}
-                </div>
+                    <div class="page__main__flat__container__item__body">
 
-                <div class="page__main__flat__container__item__body">
+                        <img src="/img/flats/flat0{{rand(1,4)}}.jpeg" alt="" srcset="">
+                        <div><strong>Address:</strong></div>
+                        <div>{{$building->street}} {{$building->house_number}}, {{$building->city}}</div>
+                        <div><strong>Located on floor:</strong></div>
+                        <div>{{$one_flat->floor}}</div>
+                        <div><strong>Type of use:</strong></div>
+                        <div>{{$one_flat->residential?"Residential":"Commercial"}}</div>
+                        <div><strong>Square area:</strong></div>
+                        <div>{{rand(25,50)}} m2</div>
+                        <div><strong>Rooms:</strong></div>
+                        <div>{{rand(1,4)}} </div>
 
-                    <img src="/img/flats/flat0{{rand(1,4)}}.jpeg" alt="" srcset="">
-                    <table>
-
-                        <tr>
-                            <td>Adress</td>
-                            <td>{{$building->city}}</td>
-                            <td> {{$building->street}} {{$building->house_number}}</td>
-                        </tr>
-                        <tr>
-                            <td>floor:</td>
-                            <td>{{$one_flat->floor}}</td>
-                        <tr>
-                            <td>type:</td>
-                            <td>{{$one_flat->residential?"Residential":"Commercial"}}
-                        </tr>
-
-                        <tbody>
-                        </tbody>
-
-                    </table>
-
+                    </div>
                 </div>
                 @endif
                 @endforeach
 
+
             </div>
             @endforeach
-
         </div>
+
+
 
 
 
